85950f5daf7d75c104a9425dc0ee10cb0aa345eb
Abdul Khan <akhan17@outlook.com>
2020-10-22 19:58:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /* Your Code Here */
-function createEmployeeRecord(){
-  return {
-      firstName: data[0],
-      familyName: data[1],
-      title: data[2],
-      payPerHour: data[3],
-      timeInEvents: [],
-      timeOutEvents: [],
-  }
+let createEmployeeRecord = function (data) {
+    return {
+        firstName: data[0],
+        familyName: data[1],
+        title: data[2],
+        payPerHour: data[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
 }
 
 let createEmployeeRecords = function (dataSet) {
