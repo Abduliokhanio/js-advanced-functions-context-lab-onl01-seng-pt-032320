520fb0eb709314974b1059d867c723fb3c6f1b00
Abdul Khan <akhan17@outlook.com>
2020-10-22 19:58:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ let createEmployeeRecords = function (dataSet) {
         return createEmployeeRecord(data)
     })
 }
+
+let createTimeInEvent = function (timeStamp) {
+    let [date, hour] = timeStamp.split(' ')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return this
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
